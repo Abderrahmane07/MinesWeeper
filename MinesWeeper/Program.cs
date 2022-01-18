a56743722cac31e9a297b3344fb4862fd2402490
Abderrahmane07 <75677650+Abderrahmane07@users.noreply.github.com>
2022-01-18 19:12:13 +0100

    First part

---
+++ MinesWeeper/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MinesWeeper
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            bool[] n = new bool[100];
+
+            for (int i = 0; i < 90; i++)
+                n[i] = false;
+
+            for (int i = 90; i < 100; i++)
+                n[i] = true;
+
+            for(int i=0; i<100; i++)
+            {
+                int pos = rand.Next(100);
+                bool save = n[i];
+                n[i] = n[pos];
+                n[pos] = save;
+            }
+
+            for(int i = 0; i < 100; i++)
+            {
+                if (n[i])
+                    Console.Write("* ");
+                else
+                    Console.Write(". ");
+                if((i+1)%10==0)
+                    Console.WriteLine();
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
